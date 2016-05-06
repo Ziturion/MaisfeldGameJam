e66ce5d43d5a98862892970b52ba712864e2a5a5
Tina <Tina@Plavius.de>
2016-05-06 20:17:03 +0200

    TinasScripts

---
+++ MaisfeldSimulator3000/Assets/Scripts/Take_Item.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Take_Item : MonoBehaviour {
+
+    public GameObject Player;
+    public int PickDistance = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+
+
+        if (Vector3.Distance(this.transform.position, Player.transform.position) < PickDistance)
+        {
+            Destroy(this.gameObject);
+        }
+
+        //save object as taken
+
+    }
+
+}
