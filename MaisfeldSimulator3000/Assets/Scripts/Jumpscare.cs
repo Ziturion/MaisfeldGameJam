372957d6ae6c719e22022c04fb99c70318baf075
Tina <Tina@Plavius.de>
2016-05-06 20:32:53 +0200

    Jumpscare script

---
+++ MaisfeldSimulator3000/Assets/Scripts/Jumpscare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumpscare : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+
+
+		if (other.GetComponent<GameObject> () == Player) {
+			//TODO Was soll passieren
+		}
+
+	}
+}
