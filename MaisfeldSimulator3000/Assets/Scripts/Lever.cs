b1506e9c6531f51c59e760bec51dfd5f230c9327
Ziturion <dmajonica@aol.com>
2016-05-07 12:31:33 +0200

    LeverLogic

---
+++ MaisfeldSimulator3000/Assets/Scripts/Lever.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject Player;
+    public int PickDistance = 5;
+    public bool Triggered = false;
+
+    void OnMouseDown()
+    {
+        TriggerLogic();
+    }
+    public void TriggerLogic()
+    {
+        if (Vector3.Distance(this.transform.position, Player.transform.position) < PickDistance)
+        {
+            this.transform.Rotate(this.transform.localRotation.x, this.transform.localRotation.y, this.transform.localRotation.z + 180, Space.Self);
+            if (Triggered)
+            {
+                Triggered = false;
+            }
+            else
+            {
+
+                Triggered = true;
+                Player.GetComponent<Leverscript>().PressedLever();
+            }
+        }
+    }
+}
