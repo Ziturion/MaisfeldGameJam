2772ab35358719e3dabc008a8db06ebb27953c7b
Ziturion <dmajonica@aol.com>
2016-05-07 15:25:15 +0200

    Added Horse and Gate (Key) mechanic

---
+++ MaisfeldSimulator3000/Assets/Scripts/KeyGate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class KeyGate : MonoBehaviour {
+
+    public GameObject Player;
+    public int PickDistance = 5;
+
+    void OnMouseDown()
+    {
+
+
+        if (Vector3.Distance(this.transform.position, Player.transform.position) < PickDistance)
+        {
+            if(Player.GetComponent<FirstPersonController>().GethasKey())
+            {
+                Destroy(this.gameObject);
+            }
+            
+        }
+
+        //save object as taken
+
+    }
+}
