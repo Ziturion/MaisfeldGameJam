0f9bb1980ace38176d7b0d89f93ac0010e038a85
Ziturion <dmajonica@aol.com>
2016-05-07 09:09:12 +0200

    Added Maze Scene und Bereich vor der Farm.

---
+++ MaisfeldSimulator3000/Assets/Editor/SetRandomRotationandScaleEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SetRandomRotationandScale))]
+[CanEditMultipleObjects]
+public class SetRandomRotationandScaleEditor : Editor {
+
+    float MinScale = 1f, MaxScale = 2f;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SetRandomRotationandScale myScript = (SetRandomRotationandScale)target;
+        if (GUILayout.Button("Set Random Rotation"))
+        {
+            myScript.Randomrotation();
+        }
+        EditorGUILayout.MinMaxSlider(ref MinScale, ref MaxScale, 1,3);
+        if (GUILayout.Button("Set Random Scale"))
+        {
+            myScript.RandomScale(MinScale,MaxScale);
+        }
+    }
+}
---
+++ MaisfeldSimulator3000/Assets/Scripts/SetRandomRotationandScale.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRandomRotationandScale : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Randomrotation()
+    {
+        this.transform.Rotate(0,Random.Range(0,180),0,Space.Self);
+    }
+    public void RandomScale(float minscale,float maxscale)
+    {
+        this.transform.localScale = new Vector3(Random.Range(minscale, maxscale), Random.Range(minscale, maxscale), Random.Range(minscale, maxscale));
+    }
+}
