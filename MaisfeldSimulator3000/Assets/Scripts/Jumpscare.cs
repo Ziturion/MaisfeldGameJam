c0bac27d0a48e40f3211e8df518bd912f5d99d7f
Ziturion <dmajonica@aol.com>
2016-05-07 21:08:03 +0200

    added Jumpscare to MaizeScene

--- MaisfeldSimulator3000/Assets/Scripts/Jumpscare.cs
+++ MaisfeldSimulator3000/Assets/Scripts/Jumpscare.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Jumpscare : MonoBehaviour {
 
 	public GameObject Player;
+    public bool Konsistent = true;
+    public float DurationWhenNotKonsistent = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +19,16 @@ public class Jumpscare : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
 
-
-		if (other.GetComponent<GameObject> () == Player) {
-			//TODO Was soll passieren
-		}
-
+        this.GetComponent<MeshRenderer>().enabled = true;
+            if (!Konsistent)
+            {
+                StartCoroutine(Deactivate());
+            }
 	}
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(DurationWhenNotKonsistent);
+        this.gameObject.SetActive(false);
+    }
 }
