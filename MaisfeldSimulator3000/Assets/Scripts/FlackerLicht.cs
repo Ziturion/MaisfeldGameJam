aa9230039ca1253a1fd38fc0260487a255f11f48
Ziturion <dmajonica@aol.com>
2016-05-07 20:11:29 +0200

    added FlackerLicht.cs

---
+++ MaisfeldSimulator3000/Assets/Scripts/FlackerLicht.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlackerLicht : MonoBehaviour {
+    private Light licht;
+    // Use this for initialization
+    void Start () {
+
+        licht = GetComponent<Light>();
+}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time % Random.Range(1, 3) == 0)
+        {
+            licht.intensity = Random.Range(2, 7);
+        }
+	
+	}
+}
