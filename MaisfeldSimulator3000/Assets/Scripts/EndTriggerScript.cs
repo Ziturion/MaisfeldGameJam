35da77c1fd1c3a349f7d6a3cbe051c8f7981d2ed
Tina <Tina@Plavius.de>
2016-05-08 10:42:56 +0200

    quit on playthrough

--- MaisfeldSimulator3000/Assets/Scripts/EndTriggerScript.cs
+++ MaisfeldSimulator3000/Assets/Scripts/EndTriggerScript.cs
@@ -87,7 +87,7 @@ public class EndTriggerScript : MonoBehaviour {
                 yield return new WaitForSeconds(0.05f);
             }
         yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(0);
+        Application.Quit();
 
         }
 
