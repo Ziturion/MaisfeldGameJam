528a2e0e4d3c955291504cbe1f1e330378ce3f65
Ziturion <dmajonica@aol.com>
2016-05-08 10:39:39 +0200

    Added Story Text elements

--- MaisfeldSimulator3000/Assets/Scripts/BlendInItemText.cs
+++ MaisfeldSimulator3000/Assets/Scripts/BlendInItemText.cs
@@ -7,7 +7,7 @@ public class BlendInItemText : MonoBehaviour {
 
     bool hasWatched = false;
     public enum TextItemType{
-        Bogen,Tomahawk,Federhut,Friedenspfeife
+        Bogen,Tomahawk,Federhut,Friedenspfeife,Anfang, Pferd, Keller, Schalter, Kultraum
     }
     public TextItemType ThisItem;
     public Text Textfeld;
@@ -46,6 +46,16 @@ public class BlendInItemText : MonoBehaviour {
                 return "Der alte Schamane ist schon länger verschwunden, kann es sein, dass er auf diesem Hof verschwunden ist? Haben sie ihn hier gefangen genommen? Ich bitte die Geister darum, dass “Weißer Biber” noch lebt. Was geht hier nur vor sich?";
             case TextItemType.Tomahawk:
                 return "Wieso ist das Tomahawk von meinem Bruder “Der mit dem Huf stampft” hier? Haben sie ihn etwa auch gefangen? Wer sind diese Wesen? Hat unser Stamm das Wohlwollen der Geister verloren? Dies sind böse Omen auf eine dunkle Zeit.";
+            case TextItemType.Anfang:
+                return "Uff… Mir schmerzt alles, wo bin ich? Wo ist mein Pferd? Ich muss zurück zu meinem Stamm, irgendwas stimmt hier nicht. Wer hat mich wohl hier hergebracht und wie einen Toten liegen gelassen?";
+            case TextItemType.Kultraum:
+                return "Was für ein grauenhafter Anblick, was für böse Zauber beschwören die Leute hier?! Ich sollte schleunigst den Schlüssel holen und von hier verschwinden. Ob das hier die Gräber meiner Brüder und Schwestern sind?";
+            case TextItemType.Keller:
+                return "Oben war kein Schlüssel zum Stall, vielleicht ist er im Keller… Hmm… Das ist ja ein großer Stollen, was geht hier vor sich?";
+            case TextItemType.Schalter:
+                return "Ob diese Hebel etwas mit dem Torbogen zu tun haben?"; 
+            case TextItemType.Pferd:
+                return "Windmähne, da bist du ja! Wer hat dich hier eingesperrt in diesem engen Käfig? Warte nur, ich suche den Schlüssel und befreie dich, dann fliehen wir von diesem schaurigen Acker!"; 
         }
         return "";
     }
