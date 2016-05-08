03eee9904ea33878d9dc0d520b6b5b8b35be7a4f
Tina <Tina@Plavius.de>
2016-05-08 11:11:24 +0200

    changes in dungeon light sound

--- MaisfeldSimulator3000/Assets/Scripts/FlackerLicht.cs
+++ MaisfeldSimulator3000/Assets/Scripts/FlackerLicht.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class FlackerLicht : MonoBehaviour {
     public float MinimumIntensity = 0, MaximumIntensity = 8, Frequence = 8;
     private Light licht;
+	public AudioSource audio;
+
     // Use this for initialization
     void Start () {
 
         licht = GetComponent<Light>();
+
 }
 	
 	// Update is called once per frame
@@ -16,6 +19,8 @@ public class FlackerLicht : MonoBehaviour {
         if (Time.time % Random.Range(1, Frequence) >= 0 && Time.time % Random.Range(1, Frequence) <= 1)
         {
             licht.intensity = Random.Range(MinimumIntensity, MaximumIntensity);
+			if(audio!= null)
+			audio.volume = licht.intensity / MaximumIntensity;
         }
 	
 	}
